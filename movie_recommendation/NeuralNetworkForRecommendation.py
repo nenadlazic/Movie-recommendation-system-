a9638be7505bb753b1a95ef0430e6b1c7a5fc9f2
nlazic <nenadlazic13@gmail.com>
2017-06-03 01:22:55 +0200

    Add parsing test data

--- movie_recommendation/NeuralNetworkForRecommendation.py
+++ movie_recommendation/NeuralNetworkForRecommendation.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 #koristimo porterov stremmer za pronalazenja korena reci radi bolje klasifikacije
 from porter2stemmer import  Porter2Stemmer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 import tensorflow as tf
 import pandas as pd
 
@@ -63,7 +64,54 @@ def GetRecommendations():
     vectorizer = CountVectorizer()
     vectorizer.fit_transform(corpus)
     print(vectorizer.vocabulary_)
+    print(len(vectorizer.vocabulary_))
 
+    #test set
+    test_set = pd.read_csv(TEST_SET, skipinitialspace=True, skiprows=1, names=COLUMNS)
+    words_set_test = test_set.ix[:,['plot_keywords', 'movie_title','people']]
+
+    x = words_set_test.ix[:,['people']]
+
+    test_list_items = []
+
+
+    for index, row in words_set_test.iterrows():
+        jedan_red_string_test = ""
+
+        m = row['movie_title'].split(' ')
+        for k in range(len(m)):
+            s = m[k]
+            jedan_red_string_test += porterStremmer.stem(s.lower())
+            jedan_red_string_test += " "
+
+
+        n = row['people'].split('|')
+        for k in range(len(n)):
+            s = n[k]
+            jedan_red_string_test += porterStremmer.stem(s.lower())
+            jedan_red_string_test += " "
+
+
+        rowrow = row['plot_keywords'].split('|')
+        for j in range(len(rowrow)):
+            p = rowrow[j].split(' ')
+            for k in range(len(p)):
+                s = p[k]
+                jedan_red_string_test += porterStremmer.stem(s.lower())
+                jedan_red_string_test += " "
+        test_list_items.append(jedan_red_string_test)
+
+    corpus_test = test_list_items
+    print(corpus_test)
+    freq_term_matrix = vectorizer.transform(corpus_test)
+    print(freq_term_matrix.todense())
+
+    tfidf = TfidfTransformer()
+    tfidf.fit(freq_term_matrix)
+    print("IDF:", tfidf.idf_)
+    vectorizer.fit_transform(corpus_test)
+    print(vectorizer.vocabulary_)
+    print(len(vectorizer.vocabulary_))
 
 
 
